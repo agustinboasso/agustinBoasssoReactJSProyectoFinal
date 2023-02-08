fe2da1902cb2528df6fb16891bfe61cd9fec4c04
Agustin Boasso <boassoagustin@gmail.com>
2023-02-08 17:08:24 -0300

    ultimando detalles 2

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -25,14 +25,14 @@ const NavBar = () => {
           style={{ maxHeight: '100px' }}
           navbarScroll
         >
-          <Nav.Link as={Link} to="/">Todo</Nav.Link>
+          
           <Nav.Link as={Link} to="category/accesories">Accesorios</Nav.Link>
           <NavDropdown title="Cartas" id="navbarScrollingDropdown">
             <NavDropdown.Item as={Link} to="category/teamRocket">Team Rocket</NavDropdown.Item>
             <NavDropdown.Item as={Link} to="category/baseSet">
               Base Set
             </NavDropdown.Item>
-            <NavDropdown.Divider />
+            
             <NavDropdown.Item as={Link} to="category/jungle">
               Jungla
             </NavDropdown.Item>
@@ -41,15 +41,8 @@ const NavBar = () => {
             Sellado
           </Nav.Link>
         </Nav>
-        <Form className="d-flex">
-          <Form.Control
-            type="search"
-            placeholder="Search"
-            className="me-2"
-            aria-label="Search"
-          />
-          <Button variant="outline-success">Search</Button>
-        </Form>
+        
+          
       </Navbar.Collapse>
     </Container>
     <div>
